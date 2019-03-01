5f41c65951b76e090427898b5d4323af9e538418
Logan Lepage <logan.lepage@outlook.com>
2019-03-01 12:25:19 +0100

    ajout de lodash sur le server

--- resources/server/server.js
+++ resources/server/server.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const _ = require('lodash');
 const server = express();
 server.use(cors());
-
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
-}
 
 /**
  * Return pairs of shuffled ids
@@ -24,9 +15,9 @@ server.get('/', (req, res) => {
   if (!nb || !size) {
     return res.status(500).send('BAD REQUEST, format your url like : localhost:8081/?nb=10&size=4')
   }
-  const imagesId = shuffle([...Array(nb).keys()]);
+  const imagesId = _.shuffle([...Array(nb).keys()]);
   const sliced = imagesId.slice(0, size);
-  const boardImagesIds = shuffle(sliced.concat(sliced));
+  const boardImagesIds = _.shuffle(sliced.concat(sliced));
   res.json({'ids': boardImagesIds});
 });
 
