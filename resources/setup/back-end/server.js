245c7ed402e1ee8a32cc88213439cb69c7bf8e43
Alex <apuret@e-biz.fr>
2019-06-10 03:15:54 +0200

    add post/get scores endpoints

--- resources/setup/back-end/server.js
+++ resources/setup/back-end/server.js
@@ -12,6 +12,14 @@ const swaggerJsdoc = require('swagger-jsdoc');
 
 const app = express();
 const specs = swaggerJsdoc(config.swagger);
+
+const scores = [];
+
+// populate scores with random scores
+scores.push({name: 'Alexis', time: _.random(999, true), size: _.random(0, 12)});
+scores.push({name: 'Nicolas', time: _.random(999, true), size: _.random(0, 12)});
+
+
 app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
@@ -51,6 +59,109 @@ app.get('/board', (req, res) => {
 
     res.json({'ids': _.shuffle([...Array(size).keys(), ...Array(size).keys()])});
 });
+
+/**
+ * @swagger
+ * /scores:
+ *    post:
+ *      summary: Post score for a player
+ *      description: Save player's score on the server.
+ *      parameters:
+ *        - in: query
+ *          name: size
+ *          schema:
+ *            type: integer
+ *          required: true
+ *          description: "Size (ie: amount of card) of the generated board."
+ *        - in: query
+ *          name: name
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: "Name of the player."
+ *        - in: query
+ *          name: time
+ *          schema:
+ *            type: float
+ *          required: true
+ *          description: "Time player spend to resolve the game."
+ *      responses:
+ *        - '201':
+ *          description: Score saved
+ *        - '400':
+ *          description: one parameter was missing.
+ */
+
+/**
+ *
+ * @nb nbImages number of images in your project
+ * @size boardSize the board size
+ * @time time player spend to resolve the game
+ * @name player name
+ */
+app.post('/scores', (req, res) => {
+    const size = req.query.size && parseInt(req.query.size);
+    const time = req.query.time && (req.query.time);
+    const name = req.query.name;
+
+    if (!size || !name || !time) {
+        return res.status(400).send('Missing parameter, size, name and time are required');
+    }
+
+    scores.push({size, name, time});
+
+    res.status(201).end();
+});
+
+
+
+/**
+ * @swagger
+ * /scores:
+ *    get:
+ *      summary: Get all saved scores
+ *      description: return a json of all scores saved
+ *      parameters:
+ *        - in: query
+ *          name: size
+ *          schema:
+ *            type: integer
+ *          required: false
+ *          description: "Size (ie: amount of card) of the generated board."
+ *        - in: query
+ *          name: name
+ *          schema:
+ *            type: string
+ *          required: false
+ *          description: "Name of the player."
+ *      responses:
+ *        - '200':
+ *          description: List returned
+ *        - '400':
+ *          description: One paramater was not well set.
+ */
+
+/**
+ *
+ * @nb nbImages number of images in your project
+ * @size boardSize the board size
+ */
+app.get('/scores', (req, res) => {
+    const size = req.query.size && parseInt(req.query.size);
+    const name = req.query.name;
+
+    let filteredScores = scores;
+    if (size) {
+        filteredScores = filteredScores.filter(score => score.size === size);
+    }
+    if (name) {
+        filteredScores = filteredScores.filter(score => score.name === name);
+    }
+
+    res.json(filteredScores);
+});
+
+
 
 app.listen(config.port, () => {
     console.log(chalk.bold.green(
